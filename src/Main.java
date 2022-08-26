2a55ff973d8978d4417f13f6e6d0526c87cb5a2e
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-26 17:26:56 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+    private static int searchYear() {
+        int year = 2022;
+        boolean leapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+        if (leapYear) {
+            System.out.println(year + "  високосный");
+            return year;
+        }
+        System.out.println(year + "  не високосный");
+        return year;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        searchYear();
     }
 }
