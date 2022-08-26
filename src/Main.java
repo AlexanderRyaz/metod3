e40ab3767eed6d3f6aa65e707e47fa57fdfd95a7
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-26 17:28:35 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
+import java.time.LocalDate;
+
 public class Main {
-    private static int searchYear() {
-        int year = 2022;
-        boolean leapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
-        if (leapYear) {
-            System.out.println(year + "  високосный");
-            return year;
+    private static int choosingVersion() {
+        int phoneOS = 1;
+        int currentYear = LocalDate.now().getYear();
+        if (phoneOS == 0 && currentYear >= 2015) {
+            System.out.println("По ссылке вы можете установить версию для iOS");
+        } else {
+            if (phoneOS == 0 && currentYear <= 2015) {
+                System.out.println("Установите Lite версию для iOS по ссылке");
+            }
         }
-        System.out.println(year + "  не високосный");
-        return year;
+        if (phoneOS == 1 && currentYear >= 2015) {
+            System.out.println("По ссылке вы можете установить версию для Android");
+        } else {
+            if (phoneOS == 1 && currentYear <= 2015) {
+                System.out.println("Установите Lite версию для Android по ссылке");
+            }
+        }
+        return phoneOS;
     }
-
-
     public static void main(String[] args) {
-        searchYear();
+        choosingVersion();
     }
 }
