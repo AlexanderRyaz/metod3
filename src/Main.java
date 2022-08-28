2e75e2615f3b9bb1d4d86261be75fc4047a1b2df
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-28 12:51:54 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
-    private static void countingDaysDelivery() {
-        int deliveryDistance = 95;
+    private static int countingDaysDelivery(int deliveryDistance) {
         int deliveryDay = 1;
-        if (deliveryDistance > 20) {
-            deliveryDay++;
+        if (deliveryDistance < 20) {
+            return deliveryDay;
+        } else {
+            int count = (deliveryDistance - 20) / 40;
+            deliveryDay += count;
+            if ((deliveryDistance - 20) % 40 != 0) {
+                deliveryDay++;
+            }
         }
-
-        if (deliveryDistance > 60) {
-            deliveryDay++;
-        }
+        return deliveryDay;
     }
+
     public static void main(String[] args) {
-        countingDaysDelivery();
+        int daysDelivery = countingDaysDelivery(101);
+        System.out.println(daysDelivery);
     }
 }
